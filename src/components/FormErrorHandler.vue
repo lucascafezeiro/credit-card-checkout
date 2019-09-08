be312d4035e676261734b2177ec86ce3e86549c6
Lucas Cafezeiro <geraldolucas@gmail.com>
2019-09-08 17:50:06 -0300

    improve Form

---
+++ src/components/FormErrorHandler.vue
@@ -0,0 +1,41 @@
+<script>
+
+const FormErrorHandler =  {
+    data () {
+        return {
+            formErrors: []
+        }
+    },
+    methods: {
+        insertError (field, message) {
+            this.formErrors.push(
+                {
+                    field : field,
+                    hasError: true,
+                    message: message
+                }
+            );
+        },
+        getError(field) {
+            const INDEX_ERROR = this.formErrors.findIndex ((error) => (error.field === field));
+            const  HAS_ERROR = INDEX_ERROR > -1;
+            if ( HAS_ERROR ) {
+                return this.formErrors[INDEX_ERROR];
+            } else {
+                return {
+                    hasError: false,
+                    message: ''
+                }
+            }
+        },
+        resetError (field) {
+            this.formErrors = this.formErrors.filter(
+                (error) => (error.field !== field)
+            )
+        }
+    }
+}
+
+export default FormErrorHandler;
+
+</script>
---
+++ src/components/VButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <button v-bind:type="type" class="button" v-on:click="$emit('click')">
+        <slot>Send</slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'VButton',
+    props: {
+        type: {
+            type: String,
+            required: false,
+            default: 'button'
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.button {
+    cursor: pointer;
+    border: none;
+    background-color: #407CE7;
+    color: #FFF;
+    font-size: 0.8em;
+    width: 80%;
+    height: 45px;
+    border-radius: 5pt;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    -webkit-box-shadow: 0px 0px 36px -5px rgba(64,125,231,1); /* Chrome all / Safari all */
+    -moz-box-shadow: 0px 0px 36px -5px rgba(64,125,231,1); /* Firefox all             */
+    box-shadow: 0px 0px 36px -5px rgba(64,125,231,1); /* Likely future           */
+    -webkit-user-select: none; /* Chrome all / Safari all */
+    -moz-user-select: none;    /* Firefox all             */
+    -ms-user-select: none;     /* IE 10+                  */
+    user-select: none;        /* Likely future           */
+}
+</style>
